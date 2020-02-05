4420133998c5b4df9cca43685ce338815de5952c
Mike Curtis <michael.curtis@bloomingdales.com>
2020-02-05 13:42:19 -0500

    really fixing issue with main landing page

--- src/components/app.js
+++ src/components/app.js
@@ -10,18 +10,21 @@ import centhrisFull from "../img/maps/Centhris-full.png";
 class App extends Component {
   render() {
     return (
-      <main className="content">
-      <Navigation/>
-        <h2 className="sectionTitle">A Wiki for the Fantasy World of Centhris</h2>
-        <article className="description">
-          { 
-            WikiUtils.linkContent({},["The world of Centhris is a homebrew world in the vast cosmos of planes that make up the Dungeons & Dragons multiverse. This wiki acts as a central point of information for the various people, places, and things that can be found upon it."])
-          }
-          <a href={centhrisFull}>
-            <img alt="World Map of Centhris" className="worldMap" src={centhris}/>
-          </a>
-        </article>
-      </main>
+      <div className="App">
+        <Navigation/>
+
+        <main className="content">
+          <article className="description">
+            <h2 className="sectionTitle">A Wiki for the Fantasy World of Centhris</h2>
+            { 
+              WikiUtils.linkContent({},["The world of Centhris is a homebrew world in the vast cosmos of planes that make up the Dungeons & Dragons multiverse. This wiki acts as a central point of information for the various people, places, and things that can be found upon it."])
+            }
+            <a href={centhrisFull}>
+              <img alt="World Map of Centhris" className="worldMap" src={centhris}/>
+            </a>
+          </article>
+        </main>
+      </div>
     );
   }
 }
