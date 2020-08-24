45de6d730ac10478f401e109ee87ee73fb8aa9cc
Mike Curtis <mallekith@gmail.com>
2020-08-24 19:10:07 -0400

    udpating and adding new npcs

--- src/data/people/allPeople.js
+++ src/data/people/allPeople.js
@@ -1,15 +1,17 @@
-import heroData             from 'data/people/heroes';
-import villainData          from 'data/people/villains';
-import nobleData            from 'data/people/nobles';
-import importantPeopleData  from 'data/people/important';
-import merchantsData        from 'data/people/merchants';
-import historicalPeopleData from 'data/people/historical';
-import mageData             from 'data/people/mages';
-import miscPeopleData       from 'data/people/misc';
-import soldiers       from 'data/people/soldiers';
-import archfeyData          from 'data/people/archfey';
-import lordsOfTheFell       from 'data/people/lordsOfTheFell';
-import witheredCouncil       from 'data/people/councilOfTheWitheredKing';
+import heroData from "data/people/heroes";
+import villainData from "data/people/villains";
+import nobleData from "data/people/nobles";
+import importantPeopleData from "data/people/important";
+import merchantsData from "data/people/merchants";
+import historicalPeopleData from "data/people/historical";
+import mageData from "data/people/mages";
+import miscPeopleData from "data/people/misc";
+import soldiers from "data/people/soldiers";
+import archfeyData from "data/people/archfey";
+import lordsOfTheFell from "data/people/lordsOfTheFell";
+import witheredCouncil from "data/people/councilOfTheWitheredKing";
+import tovanis from "data/people/tovanis";
+import lemuria from "data/people/lemuria";
 
 export default {
   ...heroData,
@@ -18,10 +20,12 @@ export default {
   ...importantPeopleData,
   ...merchantsData,
   ...historicalPeopleData,
-  ...miscPeopleData, 
-  ...mageData, 
+  ...miscPeopleData,
+  ...mageData,
   ...soldiers,
-  ...archfeyData, 
+  ...archfeyData,
   ...lordsOfTheFell,
-  ...witheredCouncil
+  ...witheredCouncil,
+  ...tovanis,
+  ...lemuria,
 };
