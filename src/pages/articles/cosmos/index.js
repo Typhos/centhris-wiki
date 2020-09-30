030e7f84587b8dad9c7cd2a5a9e789316975a2fb
Mike Curtis <mallekith@gmail.com>
2020-09-30 16:10:51 -0400

    updating more file types to reduce slug size

--- src/pages/articles/cosmos/index.js
+++ src/pages/articles/cosmos/index.js
@@ -1,25 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import WikiUtils from "components/utils/wikiUtils";
-import DataLoader from 'components/utils/dataLoader';
-import Back from 'components/back';
-import { TitleComponent } from 'components/titleComponent.js';
+import DataLoader from "components/utils/dataLoader";
+import Back from "components/back";
+import { TitleComponent } from "components/titleComponent.js";
 
-import Page from 'components/page';
-import ListItem from 'components/categories/listItem';
+import Page from "components/page";
+import ListItem from "components/categories/listItem";
 
-import banner from "img/lore/banners/cosmos-banner.png";
+import banner from "img/lore/banners/cosmos-banner.jpg";
 
 class Cosmos extends Component {
-
-  constructor (props) {
+  constructor(props) {
     super(props);
 
-    const dmView = localStorage.getItem('dmView') === 'true';
+    const dmView = localStorage.getItem("dmView") === "true";
 
-    const planes = Object.keys(DataLoader.places).filter( place => {
+    const planes = Object.keys(DataLoader.places).filter((place) => {
       const placeData = DataLoader.places[place];
       if (placeData.type === "Plane") {
-        if ( dmView || placeData.playerKnown === true ) {
+        if (dmView || placeData.playerKnown === true) {
           return true;
         }
       }
@@ -29,8 +28,8 @@ class Cosmos extends Component {
     this.state = {
       dmView: dmView,
       planes: planes,
-      articleContent: DataLoader.all["Cosmos"]
-    }
+      articleContent: DataLoader.all["Cosmos"],
+    };
 
     this.getPlanes = this.getPlanes.bind(this);
     this.checkEmptyEntry = this.checkEmptyEntry.bind(this);
@@ -42,54 +41,64 @@ class Cosmos extends Component {
     return (
       <Page.CategoryArticle>
         <TitleComponent title={`The Cosmos - Centhris Wiki`} />
-        <Back/>
+        <Back />
 
         <h1 className="article__heading">The Cosmos</h1>
-        <div style={{
-          display: "block",
-          height:"100px",
-          width: "100%",
-          marginBottom: "2em",
-          opacity: "90%",
-          backgroundImage:`url(${banner})`,
-          backgroundPosition: "center center"
+        <div
+          style={{
+            display: "block",
+            height: "100px",
+            width: "100%",
+            marginBottom: "2em",
+            opacity: "90%",
+            backgroundImage: `url(${banner})`,
+            backgroundPosition: "center center",
+          }}
+        ></div>
 
-        }}></div>
-        
         <article className="article">
           <section className="article__content">
-            { 
-              WikiUtils.linkContent(this.state.articleContent, WikiUtils.textFormatting( this.state.articleContent.description), {"paragraphName": "article__paragraph", "linkName": "article__link"} )
-            }
+            {WikiUtils.linkContent(
+              this.state.articleContent,
+              WikiUtils.textFormatting(this.state.articleContent.description),
+              { paragraphName: "article__paragraph", linkName: "article__link" }
+            )}
           </section>
         </article>
-        
-        <div className="category" style={{marginTop: "3em"}}>
-          <h2 className="category__subheading">The Known Planes <small>({numberOfArticles} { (numberOfArticles > 1 || numberOfArticles === 0) ? "Entries" : "Entry"})</small></h2>
-          <ul className="category__list">
-            {this.getPlanes(this.state.planes)}
-          </ul>
+
+        <div className="category" style={{ marginTop: "3em" }}>
+          <h2 className="category__subheading">
+            The Known Planes{" "}
+            <small>
+              ({numberOfArticles}{" "}
+              {numberOfArticles > 1 || numberOfArticles === 0 ? "Entries" : "Entry"})
+            </small>
+          </h2>
+          <ul className="category__list">{this.getPlanes(this.state.planes)}</ul>
         </div>
       </Page.CategoryArticle>
     );
   }
 
-  getPlanes (planes) {
-    return planes.sort().map( plane => {     
+  getPlanes(planes) {
+    return planes.sort().map((plane) => {
       return (
-        <ListItem key={DataLoader.places[plane].name} entry={ DataLoader.places[plane] } imgStyle="" />
-      )
+        <ListItem
+          key={DataLoader.places[plane].name}
+          entry={DataLoader.places[plane]}
+          imgStyle=""
+        />
+      );
     });
   }
 
   checkEmptyEntry(entry) {
-    if (this.state.dmView && WikiUtils.stubCheck(entry) ) {
+    if (this.state.dmView && WikiUtils.stubCheck(entry)) {
       return "empty";
     }
 
     return "";
   }
-  
 }
 
 export { Cosmos };
--- src/pages/articles/runes/index.js
+++ src/pages/articles/runes/index.js
@@ -1,26 +1,25 @@
-import React, { Component } from 'react';
-import WikiUtils from 'components/utils/wikiUtils';
-import { Link } from 'react-router-dom';
-import Page from 'components/page';
-import Back from 'components/back';
-import { TitleComponent } from 'components/titleComponent.js';
+import React, { Component } from "react";
+import WikiUtils from "components/utils/wikiUtils";
+import { Link } from "react-router-dom";
+import Page from "components/page";
+import Back from "components/back";
+import { TitleComponent } from "components/titleComponent.js";
 
-import banner from "img/lore/banners/runesmithing-banner.png";
+import banner from "img/lore/banners/runesmithing-banner.jpg";
 
-import runesArticle from 'data/runes/info';
-import allRunes from 'data/runes/allRunes';
+import runesArticle from "data/runes/info";
+import allRunes from "data/runes/allRunes";
 
-import weaponRune from 'img/runes/6.svg';
-import armorRune from 'img/runes/2.svg';
-import talismanRune from 'img/runes/41.svg';
+import weaponRune from "img/runes/6.svg";
+import armorRune from "img/runes/2.svg";
+import talismanRune from "img/runes/41.svg";
 
-import "styles/runes.scss"
+import "styles/runes.scss";
 
-const images = require.context('img/runes/', true);
+const images = require.context("img/runes/", true);
 
 class Runes extends Component {
-
-  constructor (props) {
+  constructor(props) {
     super(props);
 
     this.state = {
@@ -29,7 +28,7 @@ class Runes extends Component {
       runeGroups: Object.keys(allRunes),
       itemRunes: [],
       cost: 0,
-      dmView: localStorage.getItem('dmView') === 'true'
+      dmView: localStorage.getItem("dmView") === "true",
     };
 
     // this.getRuneList = this.getRuneList.bind(this);
@@ -39,73 +38,83 @@ class Runes extends Component {
 
   // TODO: Update this to match the standards of other articles written later.
 
-  render () {
+  render() {
     let rules = runesArticle["Rules of Forging"];
-    
-    let section = 
+
+    let section = (
       <section id="infoSection">
         <ul className="runeList">
-          { this.state.activeRuneList && 
-            Object.keys(this.state.activeRuneList).map( key => {
-
-              const effects = this.state.activeRuneList[key].effects.map( (e,i) => {
-                return <p key={e}>{i+1} Rune(s): {e}</p>
+          {this.state.activeRuneList &&
+            Object.keys(this.state.activeRuneList).map((key) => {
+              const effects = this.state.activeRuneList[key].effects.map((e, i) => {
+                return (
+                  <p key={e}>
+                    {i + 1} Rune(s): {e}
+                  </p>
+                );
               });
 
-              const glyphs = this.state.activeRuneList[key].svg.map( (g, i) => {
-                return <img key={g} alt="" className="runeImg" src={images('./' + g + '.svg')} />
+              const glyphs = this.state.activeRuneList[key].svg.map((g, i) => {
+                return <img key={g} alt="" className="runeImg" src={images("./" + g + ".svg")} />;
               });
 
-              if ( this.state.activeRuneList[key].found || this.state.dmView) {
-                return(
+              if (this.state.activeRuneList[key].found || this.state.dmView) {
+                return (
                   <li className="rune" key={key}>
                     <div className="images">{glyphs}</div>
                     <h3 className="runeName">
                       <span className="name">{this.state.activeRuneList[key].name}</span>
                     </h3>
                     <div className="runeEffect">{effects}</div>
-                    <div className="runeCost"><span className="num">{this.state.activeRuneList[key].baseCost}</span>gp</div>
+                    <div className="runeCost">
+                      <span className="num">{this.state.activeRuneList[key].baseCost}</span>gp
+                    </div>
                   </li>
-                )
+                );
               }
               return undefined;
-            })
-          }
+            })}
         </ul>
       </section>
+    );
 
     return (
       <Page.Article>
         <TitleComponent title="Runes - Centhris Wiki" />
 
-        <article className="dwarfRunes article" >
-          <Back/>
+        <article className="dwarfRunes article">
+          <Back />
 
           <h1 className="article__heading">Dwarven Runes</h1>
 
-          <div className="article__banner" style={{
-            backgroundImage:`url(${banner})`,
-            backgroundPosition: "center bottom"
-          }}></div>
+          <div
+            className="article__banner"
+            style={{
+              backgroundImage: `url(${banner})`,
+              backgroundPosition: "center bottom",
+            }}
+          ></div>
 
           <div className="mainContent">
-            {
-              WikiUtils.linkContent(runesArticle.entry, WikiUtils.textFormatting(runesArticle.entry.description), {"paragraphName":"article__paragraph", "linkName": "article__link"} )
-            }
-            
+            {WikiUtils.linkContent(
+              runesArticle.entry,
+              WikiUtils.textFormatting(runesArticle.entry.description),
+              { paragraphName: "article__paragraph", linkName: "article__link" }
+            )}
+
             <h2 className="article__subheading">The Types of Runes</h2>
 
             <div className="linksGroup">
               <Link className="runeGroupLink" to={`/rune-list?type=weapon-runes`}>
-                <img className="runeImg" src={weaponRune} alt=""/>
+                <img className="runeImg" src={weaponRune} alt="" />
                 <p>Weapon Runes</p>
               </Link>
               <Link className="runeGroupLink" to={`/rune-list?type=armor-runes`}>
-                <img className="runeImg" src={armorRune} alt=""/>
+                <img className="runeImg" src={armorRune} alt="" />
                 <p>Armor Runes</p>
               </Link>
               <Link className="runeGroupLink" to={`/rune-list?type=talisman-runes`}>
-                <img className="runeImg" src={talismanRune} alt=""/>
+                <img className="runeImg" src={talismanRune} alt="" />
                 <p>Talismanic Runes</p>
               </Link>
             </div>
@@ -114,22 +123,16 @@ class Runes extends Component {
 
             <section id="ruleSection">
               <ul className="ruleList">
-                {
-                  rules.map( rule => {
-                    return (
-                      <li className="rule" key={rule}>
-                        <h3>{rule.name}</h3>
-                        {
-                          rule.description.map( str => {
-                            return (
-                              <p>{ WikiUtils.textFormatting(str) }</p>
-                            )
-                          })
-                        }
-                      </li>
-                    )
-                  })
-                }
+                {rules.map((rule) => {
+                  return (
+                    <li className="rule" key={rule}>
+                      <h3>{rule.name}</h3>
+                      {rule.description.map((str) => {
+                        return <p>{WikiUtils.textFormatting(str)}</p>;
+                      })}
+                    </li>
+                  );
+                })}
               </ul>
 
               {/* <form id="runeType">
@@ -159,21 +162,18 @@ class Runes extends Component {
   showRules() {
     this.setState({
       activeRuneType: "rules",
-      activeRuneList: null
+      activeRuneList: null,
     });
-
   }
 
-  addRuneToItem (e) {
+  addRuneToItem(e) {
     let itemRunes = this.state.itemRunes;
-    itemRunes.push( e.target.closest('li').querySelector('.name').innerHTML );
+    itemRunes.push(e.target.closest("li").querySelector(".name").innerHTML);
 
     this.setState({
-
-      cost: this.state.cost + parseInt(e.target.closest('li').querySelector('.num').innerHTML ) 
+      cost: this.state.cost + parseInt(e.target.closest("li").querySelector(".num").innerHTML),
     });
   }
-
 }
 
-export {Runes};
+export { Runes };
